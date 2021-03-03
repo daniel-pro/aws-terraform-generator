88706d35fd2746795f897b55c64f9ca71a65672f
Daniel Pro <daniel.procopio@gmail.com>
2021-03-03 16:14:07 +0100

    Fixed wrong syntax and added enable_dns_support, enable_dns_hostnames and assign_generated_ipv6_cidr_block configuration options

--- templates/vpc.tf
+++ templates/vpc.tf
@@ -2,8 +2,20 @@
 {% for item in aws_vpcs %}
 resource "aws_vpc" "{{ item.name }}" {
   cidr_block       = "{{ item.cidr_block }}"
-{% if instance_tenancy is defined %}
+{% if item.instance_tenancy is defined %}
   instance_tenancy = "{{ item.instance_tenancy }}"
+{% endif %}
+
+{% if item.enable_dns_support is defined %}
+  instance_tenancy = "{{ item.enable_dns_support }}"
+{% endif %}
+
+{% if item.enable_dns_hostnames is defined %}
+  instance_tenancy = "{{ item.enable_dns_hostnames }}"
+{% endif %}
+
+{% if item.assign_generated_ipv6_cidr_block is defined %}
+  instance_tenancy = "{{ item.assign_generated_ipv6_cidr_block }}"
 {% endif %}
 
 {% if item.tags is defined %}

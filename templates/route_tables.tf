91778423c1b6775a67c8450d1d30905e10ce3893
Daniel Pro <daniel.procopio@gmail.com>
2021-03-03 16:53:10 +0100

    Added route_tables.tf

---
+++ templates/route_tables.tf
@@ -0,0 +1,21 @@
+{% if aws_route_tables is defined %}
+{% for item in aws_route_tables %}
+resource "aws_route_table" "{{ item.name }}" {
+  vpc_id     = aws_vpc.{{ item.vpc_name }}.id
+
+{% for route in item.routes %}
+  route {
+    cidr_block = "{{ route.cidr_block }}"
+    {{ route.target_type_id_definition }} = {{ route.target_resource_type }}.{{ route.target_name }}.id
+  }
+{% endif %}
+
+{% if item.tags is defined %}
+  tags = {
+{% for tag in item.tags %}
+   {{ tag.name }}  = "{{ tag.value }}",
+{% endfor %}
+  }
+{% endif %}
+
+}

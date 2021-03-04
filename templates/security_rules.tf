031ff90c4b34b9d7a82e68b6fe376c447528c650
Daniel Pro <daniel.procopio@gmail.com>
2021-03-04 12:17:42 +0100

    Added security_rules.tf

---
+++ templates/security_rules.tf
@@ -0,0 +1,44 @@
+{% if aws_security_rules is defined %}
+{% for item in aws_security_rules %}
+resource "aws_security_rule" "{{ item.name }}" {
+{% if item.name is defined %}
+   name       = "{{ item.name }}"
+{% endif %}
+{% if item.description is defined %}
+   description       = "{{ item.description }}"
+{% endif %}
+   vpc_id     = aws_vpc.{{ item.vpc_name }}.id
+    
+{% if item.ingress_rules is defined %}
+{% for ingress_rule in item.ingress_rules %}
+   ingress {
+{% if ingress_rule.description is defined %}
+     description = "{{ ingress_rule.description }}"
+{% endif %}
+     from_port   = {{ ingress_rule.from_port }}
+     to_port     = {{ ingress_rule.to_port }}
+     protocol    = "{{ ingress_rule.protocol }}"
+ 
+{% if ingress_rule.cidr_blocks is defined %}
+     cidr_blocks = [
+{% for cidr_block in ingress_rule.cidr_blocks %}
+              {{ cidr_block }},
+{% endfor %}
+                      ]
+    }
+{% endif %}
+{% endfor %}
+{% endif %}
+    
+{% if item.tags is defined %}
+   tags = {
+{% for tag in item.tags %}
+      {{ tag.name }}  = "{{ tag.value }}",
+{% endfor %}
+   }
+{% endif %}
+
+}
+   
+{% endfor %}
+{% endif %}
